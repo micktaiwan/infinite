1181a661bed8d11f7f9b505a64346b990b393338
Mickael Faivre-Maçon <micktaiwan@users.noreply.github.com>
2021-12-28 13:42:18 +0100

    add index

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-
-// import '../service/service';
+import { Lines } from '../imports/api/books/collections';
 
 import '../imports/api/books/methods';
 import '../imports/api/books/publish';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Lines.rawCollection().createIndex({ bookId: 1, layerIndex: 1, order: 1 });
 });
